828a47ba1e1b1106e048c29f82623de4965e3e81
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-13 20:20:45 -0300

    Encapsulamento

---
+++ Encapsulamento/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using Course;
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("tv", 500.00, 5);
+            Console.WriteLine(p.GetNome());
+            p.SetNome("TV 4K");
+            Console.WriteLine(p.GetNome());
+        }
+    }
+}
