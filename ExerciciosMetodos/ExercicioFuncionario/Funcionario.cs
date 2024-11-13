9e959a29fa471d8924b62832ae73819c510de957
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-13 17:02:02 -0300

    Ex 2 de metodos

---
+++ ExerciciosMetodos/ExercicioFuncionario/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace ExercicioFuncionario;
+
+public class Funcionario
+{
+    public string Nome;
+    public double SalarioBruto;
+    public double Imposto;
+
+    public double SalarioLiquido(){
+        return SalarioBruto - Imposto;
+    }
+
+    public void AumentarSalario(double porcentagem){
+        SalarioBruto = (SalarioBruto * (porcentagem/100))+SalarioBruto;
+    }
+
+    override public string ToString() {
+        return Nome + ", R$" + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture); 
+    }
+}
---
+++ ExerciciosMetodos/ExercicioFuncionario/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFuncionario // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f;
+            f = new Funcionario();
+
+            Console.WriteLine("Entre os dados do funcionario: ");
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine() ?? "vazio";
+            Console.Write("Salário Bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine() ?? "0.0",CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine() ?? "0.0",CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionario: " + f);
+            Console.WriteLine("");
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine()?? "0.0",CultureInfo.InvariantCulture);
+            f.AumentarSalario(porcentagem);
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: "+ f);
+        }
+    }
+}
