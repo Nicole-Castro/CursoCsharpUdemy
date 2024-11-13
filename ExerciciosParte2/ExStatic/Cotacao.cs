42fa3527f74e2fa93d06054e9a9bd6c300cbf231
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-13 19:03:58 -0300

    Ex Conversao de moeda

---
+++ ExerciciosParte2/ExStatic/Cotacao.cs
@@ -0,0 +1,10 @@
+﻿namespace ExStatic;
+
+public class Cotacao
+{
+    public static double Iof = 6.0;
+    public static double Valor(double cotacao, double quantValor){
+        double total = quantValor * cotacao;
+        return total + total * Iof/100.0;
+    }
+}
---
+++ ExerciciosParte2/ExStatic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using ExStatic;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a cotação do Dólar: ");
+            double cot = double.Parse(Console.ReadLine()??"0.0", CultureInfo.InvariantCulture);
+            Console.Write("Digite quantos dólares deseja comprar: ");
+            double quant = double.Parse(Console.ReadLine()??"0.0", CultureInfo.InvariantCulture);
+            Console.WriteLine("Valor a ser pago em reais = " + Cotacao.Valor(cot,quant).ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
