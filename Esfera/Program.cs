8eb7ba04ce22bd9598f4a3d7499a22653259a974
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-13 18:53:22 -0300

    Memebros estaticos

---
+++ Esfera/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Esfera // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static double Pi = 3.14;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre o valor do raio");
+            double raio = double.Parse(Console.ReadLine() ?? "0.0", CultureInfo.InvariantCulture);
+            double circ = Circunferencia(raio);
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2",CultureInfo.InvariantCulture));
+            double volume = Volume(raio);
+            Console.WriteLine("Volume: " + volume.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + Pi.ToString("F2",CultureInfo.InvariantCulture));
+        }
+
+        static double Circunferencia(double raio){
+            return 2.0 * Pi * raio;
+        }
+
+        static double Volume(double raio){
+            return 4.0 / 3.0 *Pi*raio*raio*raio;
+        }
+    }
+}
---
+++ Esfera2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Esfera2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine() ?? "0.0", CultureInfo.InvariantCulture);
+            double circ = Calculadora.Circunferencia(raio);
+            Console.WriteLine("Circunferência: " + circ.ToString("F2",CultureInfo.InvariantCulture));
+            double volume = Calculadora.Volume(raio);
+            Console.WriteLine("Volume: " + volume.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + Calculadora.Pi.ToString("F2",CultureInfo.InvariantCulture));
+        }
+
+        
+    }
+}
