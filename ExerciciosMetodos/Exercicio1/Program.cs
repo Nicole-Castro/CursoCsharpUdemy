fd10c635c6b09ff05b2b6ebab1c147221c5a7496
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-13 16:45:15 -0300

    Ex 1 de metodos

---
+++ ExerciciosMetodos/Exercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks.Dataflow;
+
+namespace Exercicio1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r;
+            r = new Retangulo();
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            Console.Write("Largura: ");
+            r.Largura = double.Parse(Console.ReadLine()?? "0.0", CultureInfo.InvariantCulture);
+            Console.Write("Altura: ");
+            r.Altura = double.Parse(Console.ReadLine()?? "0.0", CultureInfo.InvariantCulture);
+
+           
+            Console.WriteLine("AREA = " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
