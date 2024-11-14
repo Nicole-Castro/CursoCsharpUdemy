3ac3c37e0de28b191af36fd5d989e3293f7aa8aa
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-14 14:54:49 -0300

    Params

---
+++ Seção6/Params/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Course;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(2,3,4,6,8,5);
+            System.Console.WriteLine(s1);
+        }
+    }
+}
