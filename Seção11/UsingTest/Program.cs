03a4b8c3f1342a1dad0de57f3f517c5c2ad11560
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-19 16:18:13 -0300

    Using

---
+++ Seção11/UsingTest/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace UsingTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            string path = @"/home/nicole/Documentos/C#/file1.txt";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    System.Console.WriteLine(line);
+                }
+            }
+
+        }
+        catch (IOException e)
+        {
+            System.Console.WriteLine("Error");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
