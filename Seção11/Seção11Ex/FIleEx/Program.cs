86e752f121a0472e2db93cfb6861eacc57d6a822
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-19 16:48:07 -0300

    EX Seção 11

---
+++ Seção11/Seção11Ex/FIleEx/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using FIleEx.Entities;
+
+namespace FIleEx;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sourcePath = @"/home/nicole/Documentos/C#/products.csv";
+         try {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourcePath);
+                string targetFolderPath = sourceFolderPath + @"/out";
+                string targetPath = targetFolderPath + @"/summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetPath)) {
+                    foreach (string line in lines) {
+
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Products prod = new Products(name, price, quantity);
+
+                        sw.WriteLine(prod.Name + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+    }
+}
