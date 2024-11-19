8a26b38657d67a03f78de83117b0c1ec4c985934
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-19 16:02:24 -0300

    Seção 11, testando operações com arquivos

---
+++ Seção11/FileTest/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FileTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sourcePath = @"/home/nicole/Documentos/C#/file1.txt";
+        string targetPath = @"/home/nicole/Documentos/C#/file2.txt";
+
+        try{
+            FileInfo fileInfo = new FileInfo(sourcePath);
+            fileInfo.CopyTo(targetPath);
+
+            string[] lines = File.ReadAllLines(sourcePath);
+
+            foreach (string line in lines){
+                Console.WriteLine(line);
+            }
+
+        }catch(IOException e){
+            Console.WriteLine("An error ocurred");
+            System.Console.WriteLine(e.Message);
+        }
+    }
+}
