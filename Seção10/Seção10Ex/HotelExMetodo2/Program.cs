accee96a4cd491e9c7f4d1dd03545ddfe72d16bd
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-19 15:00:24 -0300

    Exercicio Criar Exceções Personalizadas Metodo Ruim maisnão tao ruim quanto o metodo 1

---
+++ Seção10/Seção10Ex/HotelExMetodo2/Program.cs
@@ -0,0 +1,45 @@
+﻿using HotelExMetodo2.Entities;
+
+namespace HotelExMetodo2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Room Number: ");
+        int number = int.Parse(Console.ReadLine());
+        System.Console.Write("Check-in date (dd/MM/yyy): ");
+        DateTime checkIn = DateTime.Parse(Console.ReadLine());
+        System.Console.Write("Check-out date (dd/MM/yyy): ");
+        DateTime checkOut = DateTime.Parse(Console.ReadLine());
+        if (checkOut <= checkIn)
+        {
+            System.Console.WriteLine("Error in reservation: Check-out date must be after Check-in date.");
+
+
+        }
+        else
+        {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.Console.WriteLine("Reservation: " + reservation);
+
+            System.Console.WriteLine();
+            System.Console.WriteLine("Enter date to update the reservation:");
+            System.Console.Write("Check-in date (dd/MM/yyy): ");
+            checkIn = DateTime.Parse(Console.ReadLine());
+            System.Console.Write("Check-out date (dd/MM/yyy): ");
+            checkOut = DateTime.Parse(Console.ReadLine());
+
+            string error = reservation.UpdateDates(checkIn, checkOut);
+            if (error != null)
+            {
+                System.Console.WriteLine("Error in reservation: " + error);
+            }
+            else
+            {
+                System.Console.WriteLine("Reservation: " + reservation);
+
+            }
+        }
+    }
+}
