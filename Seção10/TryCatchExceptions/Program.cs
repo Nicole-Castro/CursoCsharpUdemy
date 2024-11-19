a4a2860ce6d3980dcc0836a6887d409a98211cd8
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-19 14:16:45 -0300

    Try Catch exceptions

---
+++ Seção10/TryCatchExceptions/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TryCatchExceptions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(n1 / n2);
+        }catch(DivideByZeroException e){
+            Console.WriteLine("Error! " + e.Message);
+        }catch(FormatException e){
+            Console.WriteLine("Format Error! " + e.Message);
+        }
+
+
+    }
+}
