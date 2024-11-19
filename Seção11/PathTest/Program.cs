40276a4c546dd14544f3555a4ed12a3aa6df2c18
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-19 16:35:41 -0300

    Paths

---
+++ Seção11/PathTest/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PathTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+                string path = @"/home/nicole/Documentos/C#/myFolder/file1.txt";
+                System.Console.WriteLine(Path.DirectorySeparatorChar);
+                System.Console.WriteLine(Path.PathSeparator);
+                System.Console.WriteLine(Path.GetDirectoryName(path));
+                System.Console.WriteLine(Path.GetFileName(path));
+                System.Console.WriteLine(Path.GetFileNameWithoutExtension(path));
+                System.Console.WriteLine(Path.GetExtension(path));
+                System.Console.WriteLine(Path.GetFullPath(path));
+                System.Console.WriteLine(Path.GetTempPath());
+
+    }
+}
