8c261d29d1b92c0f45595314853c797a0f72336f
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-19 16:30:25 -0300

    Directories

---
+++ Seção11/DirectoriesTest/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DirectoriesTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"/home/nicole/Documentos/C#/myFolder";
+        try
+        {
+            IEnumerable<string> folder = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+            System.Console.WriteLine("FOLDERS");
+            foreach (string folderItem in folder){
+                System.Console.WriteLine(folderItem);
+            }
+            var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+            System.Console.WriteLine("FILES");
+            foreach (string filesItems in files){
+                System.Console.WriteLine(filesItems);
+            }
+
+            Directory.CreateDirectory(path + @"/newfolder");
+        }
+        catch (IOException e)
+        {
+            System.Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ Seção11/StreamWriterTest/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace StreamWriterTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sourcePath = @"/home/nicole/Documentos/C#/file1.txt";
+        string targetPath = @"/home/nicole/Documentos/C#/file2.txt";
+        try
+        {
+            string[] lines = File.ReadAllLines(sourcePath);
+
+            using (StreamWriter sw = File.AppendText(targetPath))
+            {
+                foreach (string line in lines)
+                {
+                    sw.WriteLine(line.ToUpper());
+                }
+
+            }
+
+        }
+        catch (IOException e)
+        {
+            System.Console.WriteLine("Error");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
