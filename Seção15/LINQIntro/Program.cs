eb4a59b5dd40ed35f53f3441cdf6c63ac3eb5601
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-21 19:04:56 -0300

    Linq juntamente a Lambda

---
+++ Seção15/LINQIntro/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LINQIntro;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //specufy the data source
+        int[] numbers = new int[] { 1, 2, 3, 4, 5};
+
+        //define the query expression
+        IEnumerable<int> result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+        //Execute the query
+        foreach (int x in result){
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ Seção15/LinqLambda/Entities/Category.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinqLambda.Entities
+{
+    public class Category
+    {
+       public int Id { get; set; } 
+       public string Name { get; set; }
+       public int Tier { get; set; }
+
+
+       
+    }
+}
