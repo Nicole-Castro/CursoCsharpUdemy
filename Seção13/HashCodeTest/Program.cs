8b7f0615a412c1ba1509e8f28231b5455fef4318
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-21 14:19:32 -0300

    GetHashCode e equals

---
+++ Seção13/HashCodeTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using HashCodeTest.Entities;
+
+namespace HashCodeTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Client a = new Client { Name = "Maria", Email = "maria@gmail.com"};
+        Client b = new Client { Name = "Joao", Email = "maria@gmail.com"};
+
+        System.Console.WriteLine(a.Equals(b));
+        System.Console.WriteLine(a.GetHashCode() + " " +b.GetHashCode());
+    }
+}
