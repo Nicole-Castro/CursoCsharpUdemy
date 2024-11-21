34d28d114474ae7dc0f47c757f44bc2a6d80f4b9
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-21 15:26:55 -0300

    Dictionary

---
+++ Seção13/Dictionaries/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Dictionaries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+        cookies["user"] = "Maria";
+        cookies["email"] = "maria@gmail.com";
+        cookies["phone"] = "9994924249";
+        cookies["phone"] = "3141441341";
+
+        System.Console.WriteLine(cookies["phone"]);
+        System.Console.WriteLine(cookies["email"]);
+
+        cookies.Remove("email");
+
+        if (cookies.ContainsKey("email")){
+            System.Console.WriteLine(cookies["email"]);
+        }else{
+            System.Console.WriteLine("There is no email key");
+        }
+
+        System.Console.WriteLine("Size: " + cookies.Count);
+
+        System.Console.WriteLine("All cookies: ");
+        foreach(KeyValuePair<string, string> item in cookies){
+            System.Console.WriteLine(item.Key + " = " + item.Value);
+        }
+
+
+    }
+}
