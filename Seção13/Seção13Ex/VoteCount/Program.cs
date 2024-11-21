6ac883c3530361b7a523192c2c231cb978666ac6
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-21 15:48:39 -0300

    Ex

---
+++ Seção13/Seção13Ex/VoteCount/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace VoteCount;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter file full path: ");
+        string path = Console.ReadLine();
+        try
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                Dictionary<string, int> dict = new Dictionary<string, int>();
+
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(',');
+                    string name = line[0];
+                    int votes = int.Parse(line[1]);
+
+                    if (dict.ContainsKey(name)){
+                        dict[name] += votes;
+                    }else{
+                        dict[name] = votes;
+                    }
+                }
+                foreach (KeyValuePair<string, int> items in dict)
+                {
+                    Console.WriteLine(items.Key + ": " + items.Value);
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+    }
+}
