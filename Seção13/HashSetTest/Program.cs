6e0737ab7060fa595a2d83bf30bbf101d6e97840
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-21 14:37:33 -0300

    HashSet e SortedSet

---
+++ Seção13/HashSetTest/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HashSetTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<string> set = new HashSet<string>();
+
+        set.Add("Maria");
+        set.Add("Joao");
+        set.Add("Pedro");
+
+
+        System.Console.WriteLine(set.Contains("Maria"));
+
+        foreach (string s in set){
+            System.Console.WriteLine(s);
+        }
+    }
+}
---
+++ Seção13/SortedSetTest/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SortedSetTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SortedSet<int> s1 = new SortedSet<int>(){0,6,4,5,3,7};
+        SortedSet<int> s2 = new SortedSet<int>() {4,7,1,2,3};
+        PrintCollection(s1);
+
+        SortedSet<int> s3 = new SortedSet<int>(s1);
+
+        //union
+        s3.UnionWith(s2);
+        PrintCollection(s3);
+
+        //intersection
+        SortedSet<int> s4 = new SortedSet<int>(s1);
+        s4.IntersectWith(s2);
+        PrintCollection(s4);
+
+        //difference
+        SortedSet<int> s5 = new SortedSet<int>(s1);
+        s5.ExceptWith(s2);
+        PrintCollection(s5);
+
+    }
+
+    static void PrintCollection<T>(IEnumerable<T> collection){
+        foreach(T item in collection){
+            Console.Write(item + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
