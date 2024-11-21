b62fd08d0d47eb13b45c3cd3331007796c69fc89
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-21 16:00:57 -0300

    Extensions

---
+++ Seção13/ExtensionTest/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ExtensionTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+
+        System.Console.WriteLine(dt.ElapsedTime());
+
+        string s1 = "Good morning dear";
+
+        System.Console.WriteLine(s1.Cut(10));
+    }
+}
